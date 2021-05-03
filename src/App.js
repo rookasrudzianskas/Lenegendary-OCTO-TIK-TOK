77d243b024aa9fbf5b0b17e48a474272311531ff
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 21:33:18 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import Video from "./components/Video";
 
 function App() {
   return (
     <div className="app">
       <h1>Hello, World! 🚀</h1>
 
-
+        <div className="app__videos">
+            <Video />
+            <Video />
+            <Video />
+            <Video />
+            <Video />
+            <Video />
+            <Video />
+            <Video />
+        </div>
     </div>
   );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Video.css";
+
+const Video = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Video;
