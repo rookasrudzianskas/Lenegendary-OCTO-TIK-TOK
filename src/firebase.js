8bfd661c511a5ee171d4de2c503d3f21bf81a807
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 09:54:41 +0300

    Rokas

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCCilOF_eUfNNoFJenEDF42wm3C4wr6DxQ",
+    authDomain: "tik-tok-clone-4194c.firebaseapp.com",
+    projectId: "tik-tok-clone-4194c",
+    storageBucket: "tik-tok-clone-4194c.appspot.com",
+    messagingSenderId: "812408339263",
+    appId: "1:812408339263:web:a21bb99e1a5a7b740f718a",
+    measurementId: "G-JW44J2L9HD"
+};
