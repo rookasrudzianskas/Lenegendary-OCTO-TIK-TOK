08a595509270b57ad24a2050ad310826bf3ba887
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 08:42:52 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
 import Video from "./components/Video";
-import React from "react";
+import React, {useState, useEffect} from "react";
+import db from './firebase';
 
 function App() {
-  return (
+// stores the videos
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+    //    fire once then component loads, but never after
+        db.collection("videos").onSnapshot((snapshot) =>
+            // it takes the snapshot of what is in the firebase collection videos, set to the videos state, the posts id's
+            // it mapsper them one more time, meaning goes per each one for one more time, and gets all the data for each one, and sets in the
+            // mapping function below as the props that data, etc url, channel and so on
+            setVideos(snapshot.docs.map((doc) => doc.data()))
+        );
+    }, []);
+
+
+    return (
     <div className="app">
         {/* this is the differnt videos*/}
         <div className="app__videos">
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
-            <Video url="https://www.youtube.com/embed/AIWgvL7tLfI" channel="rookasrudzianskas" description="Wow this is amazing" song="catch My disease" likes={300} shares={343} messages={23213}/>
+            {/* looping though each video in videos state, and showing thee following data*/}
+            {videos.map(({ url, channel, description, likes, shares, messages, song}) => (
+                <Video url={url} channel={channel} song={song} likes={likes} messages={messages} description={description} shares={shares}/>
+            ))}
+
         </div>
     </div>
   );
