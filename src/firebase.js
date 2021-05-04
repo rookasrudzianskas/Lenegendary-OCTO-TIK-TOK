194bcfc81ed4c037c4c9c589ee302d1da58a8dcc
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 08:25:21 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCCilOF_eUfNNoFJenEDF42wm3C4wr6DxQ",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:812408339263:web:a21bb99e1a5a7b740f718a",
     measurementId: "G-JW44J2L9HD"
 };
+
+// get access to the firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// go to the firebaseAPp and take the firestore
+const db = firebaseApp.firestore();
+
+export default db;
