ea6ffcc4beb7790bd0d008dfe7383b632602d608
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 06:42:39 +0300

    Rokas

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,15 +1,32 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import "./styles/Video.css";
 
 const Video = () => {
+    // be default video is going not to be playing
+    const [play, setPlay] = useState(false)
     // defining the reference, we attach the video ref to the video
     const videoRef = useRef(null);
+
+    const onVideoPress = () => {
+        // if video is playing, so we pause the video
+        if(play) {
+            // if the video is playing we set the video to stop, and sen the state, to false, that video is not playing anymore
+            videoRef.current.pause();
+            setPlay(false);
+        } else {
+            // we get the current video reference and just play
+            videoRef.current.play();
+            // and set the state to true, because the video is playing
+            setPlay(true);
+        }
+
+    }
 
     return (
         <div className="video">
 
 
-            <video loop ref={videoRef}>
+            <video onClick={onVideoPress} loop ref={videoRef}>
                 <source src="http://media.w3.org/2010/05/sintel/trailer.mp4" type='video/mp4' />
             </video>
 
