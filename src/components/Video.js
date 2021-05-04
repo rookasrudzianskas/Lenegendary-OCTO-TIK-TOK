05ba05a33c3caf642b7f0716117f8e077e97fcc5
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 06:22:14 +0300

    Rokas

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,10 +4,14 @@ import "./styles/Video.css";
 const Video = () => {
     return (
         <div className="video">
-            <h1>ROOOKAS</h1>
-        {/*<video className="video__player" src=""/>*/}
 
-        {/*    video footer     */}
+            <iframe  className="video__player" width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+
+            {/*<video src=""></video>*/}
+
+
+            {/*    video footer     */}
         {/*     video sidebar   */}
         </div>
     )
