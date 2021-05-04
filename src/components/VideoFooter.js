d118f7ecb181bee8d78dbe1fe78facd0de62202a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 07:25:12 +0300

    Rokas

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./styles/VideoFooter.css";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import Ticker from "react-ticker";
 
 const VideoFooter = () => {
     return (
@@ -7,6 +9,22 @@ const VideoFooter = () => {
             <div className="videoFooter__text">
                 <h3>@rookasrudzianskas</h3>
                 <p>this is a  description</p>
+                <div className="videoFooter__ticker">
+                    <MusicNoteIcon className="videoFooter__icon" />
+
+                    {/* this is sliding text animation*/}
+                    {/* index, is which number is going on now*/}
+                    {/* smooth, then one finishes, another starts*/}
+                    <Ticker mode="smooth">
+
+                        {({ index }) => (
+                            <>
+                                <p>YOOO WHAT IS UPs</p>
+                            </>
+                        )}
+
+                    </Ticker>
+                </div>
             </div>
 
             <img className="videoFooter__record" src="https://static.thenounproject.com/png/934821-200.png" alt=""/>
